45598c83af9ec96a5b99411ee7ecec7819902230
oleg <oleg_09-95@mail.ru>
2026-07-07 20:53:04 +0300

    1:28:00 Добавлен обработчик к форме логина

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPage {}
+export class LoginPage {
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null),
+  });
+
+  onSubmit (event: Event) {
+    console.log(event);
+    console.log(this.form.value);
+  }
+}
