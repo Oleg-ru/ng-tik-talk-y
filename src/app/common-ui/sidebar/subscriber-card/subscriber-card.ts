af0521044aaa0d72a77937ee55c70c6223b5c98f
oleg <oleg_09-95@mail.ru>
2026-07-13 19:59:43 +0300

    2:34:00 Сделан nav в sidebar

---
+++ src/app/common-ui/sidebar/subscriber-card/subscriber-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscriber-card',
+  imports: [],
+  templateUrl: './subscriber-card.html',
+  styleUrl: './subscriber-card.scss',
+})
+export class SubscriberCard {}
