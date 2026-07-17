9ec4b5d3733ef36f5c9e3fb6edfeba5bb08427c7
oleg <oleg_09-95@mail.ru>
2026-07-17 21:01:26 +0300

    3:19:00 Добавлена кнопка редактировать в профиле

--- src/app/pages/profile-page/profile-page.ts
+++ src/app/pages/profile-page/profile-page.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { ProfileHeader } from '../../common-ui/profile-header/profile-header';
 import { ProfileService } from '../../data/services/profile';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
+import { SvgIcon } from '../../common-ui/svg-icon/svg-icon';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [ProfileHeader, AsyncPipe],
+  imports: [ProfileHeader, AsyncPipe, SvgIcon, RouterLink],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.scss',
 })
